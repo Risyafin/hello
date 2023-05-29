3ea4127a347c47e11d470b267786566b7e35e1bc
Risyafin <risyafin@gmail.com>
2023-05-29 09:19:44 +0700

    Kalkulator

--- main.go
+++ main.go
@@ -2,7 +2,47 @@ package main
 
 import "fmt"
 
+func Penjumlahan(a int, b int) int {
+	return a + b
+}
+func Pengurangan(a int, b int) int {
+	return a - b
+}
+func Perkalian(a int, b int) int {
+	return a * b
+}
+
+func pembagian(a int, b int) int {
+	return a / b
+}
+
 func main() {
-	fmt.Println("siapa")
-	fmt.Println("aku")
+	var numberOne int
+	var Operator string
+	var numberTwo int
+	var choice = "y"
+	fmt.Println("---- Selamat datang di Kalkulator Sederhana ----")
+	for choice == "y" {
+		fmt.Print("Masukka bilangan pertama : ")
+		fmt.Scan(&numberOne)
+		fmt.Print("Masukkan operator : ")
+		fmt.Scan(&Operator)
+		fmt.Print("Masukkan bilangan kedua : ")
+		fmt.Scan(&numberTwo)
+		if Operator == "+" {
+			fmt.Println("Hasil : ", Penjumlahan(numberOne, numberTwo))
+		} else if Operator == "-" {
+			fmt.Println("Hasil : ", Pengurangan(numberOne, numberTwo))
+		} else if Operator == "*" {
+			fmt.Println("Hasil : ", Perkalian(numberOne, numberTwo))
+		} else if Operator == "/" {
+			fmt.Println("Hasil : ", pembagian(numberOne, numberTwo))
+		} else {
+			fmt.Println("Operator yang anda masukkan salah")
+		}
+		choice = "t"
+		fmt.Println("Apakah anda ingin menggunakan kalkulator lagi ? (y/t)")
+		fmt.Scan(&choice)
+
+	}
 }
